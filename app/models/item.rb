cc87cd8aa6a7b58ca1118cb5da6d764bd001f123
hungmi <hungmihungmi@gmail.com>
2017-04-14 19:15:56 +0800

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,25 @@ class Item < ActiveRecord::Base
   end
 
   def clipboard_picnum
-  	result = self.picnum.gsub("+","")
+  	result = self.picnum#.gsub("+","")
   	first = self.location
-  	second = result.scan(/\D+/).join("\r\n\"")
-  	third = result.scan(/[0-9]+/).join("\r\n\"")
-  	return result = first + "\t\"" + second + "\t\"" + third
+    names = []
+    picnums = []
+    result.split("+").each do |name_and_picnum|
+      picnum = name_and_picnum[/[0-9]{4,}/]
+      picnums << picnum
+      names << name_and_picnum.gsub(picnum, "").to_s
+    end
+    return result = first + "\t\"" + names.join("\r\n\"") + "\t\"" + picnums.join("\r\n\"")
+   #  picnums = self.picnum.scan(/[0-9]{4,}/)
+   #  picnums.each do |ps|
+   #    result = result.gsub(ps, "")
+   #  end
+   #  item_result.split("+")
+  	# second = result.scan(/[0-9]{0,3}[.][0-9]{0,3}\D+/).join("\r\n\"") #.scan(/[0-9]+.[0-9]+\D+/).join("\r\n\"")
+  	# third = result.scan(/[0-9]+{4,}/).join("\r\n\"") #.scan(/[0-9]+.[0-9]+/).join("\r\n\"")
+   #  binding.pry
+  	# return result = first + "\t\"" + second + "\t\"" + third
   end
 
 end
